52f51b9c6af0ea41c8219de9bac929741ae64d05
Alcatraz9 <rishabhsun.71199@gmail.com>
2021-08-15 14:29:31 +0530

    Updates

---
+++ precache-manifest.e7911ae778ed11a40604411dc3c94086.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e23e9e9e420adc36e94fd2e078bca6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9db3f230d94bd8eb69c1",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "47048900d4b1e491e82c",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "0dd9a4eb292e665613c1",
+    "url": "/static/css/main.58256005.chunk.css"
+  },
+  {
+    "revision": "46c841fd92a3773d4535",
+    "url": "/static/js/2.35338e70.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.35338e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db3f230d94bd8eb69c1",
+    "url": "/static/js/3.5de6702c.chunk.js"
+  },
+  {
+    "revision": "47048900d4b1e491e82c",
+    "url": "/static/js/4.68a9a597.chunk.js"
+  },
+  {
+    "revision": "0dd9a4eb292e665613c1",
+    "url": "/static/js/main.1f832960.chunk.js"
+  },
+  {
+    "revision": "2b0198c95e7d9db1c994",
+    "url": "/static/js/runtime-main.7fe06098.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "34ebdaad90e6972aedd4632f4bd910b8",
+    "url": "/static/media/DAVLogo.34ebdaad.png"
+  },
+  {
+    "revision": "d103a32fc92a81d371e4f4161629b221",
+    "url": "/static/media/FACHIONELogo.d103a32f.png"
+  },
+  {
+    "revision": "b9f8de6119a029105630081fb2204b49",
+    "url": "/static/media/GETREKTLogo.b9f8de61.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1229770425d1e5eb34de3e1134bf76f7",
+    "url": "/static/media/NITRLogo.12297704.png"
+  },
+  {
+    "revision": "9eae59808c0be77358201af3f8bdf0c9",
+    "url": "/static/media/NTSELogo.9eae5980.png"
+  },
+  {
+    "revision": "cfea21f2fd09e97af85036af2ccc18fe",
+    "url": "/static/media/SIEVELogo.cfea21f2.png"
+  },
+  {
+    "revision": "d700b3eeb51a67be758079b727bdbeb9",
+    "url": "/static/media/TSLogo.d700b3ee.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
